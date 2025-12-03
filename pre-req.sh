f0b40b1069fd59d78d85b783015ea20b09e58e0b
freewillrv <rahulv.pro@gmail.com>
2025-12-03 13:10:39 +0530

    Adding pre-req shell commands for docker images

---
+++ pre-req.sh
@@ -0,0 +1,15 @@
+# Docker Images Setup
+sudo groupadd docker;
+sudo usermod -aG docker ${USER};
+newgrp docker;
+docker pull mysql:8.0;
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=mysqlpassword -p 3306:3306 -d mysql:8.0
+
+# MySQL Setup a schema
+CREATE SCHEMA `movie_rental` DEFAULT CHARACTER SET utf16 ;
+
+
+#Common commands
+#Restart Docker Daemon
+sudo systemctl restart docker
+
