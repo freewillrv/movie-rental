e36a47c70792115462601a083ae9fa0ba334d413
Prateek Chaturvedi <prateek@Prateeks-MacBook-Air.local>
2025-12-09 12:52:57 +0530

    unwrapped_the_data_field

--- src/main/java/com/rahul/verma/movierental/dto/ResponseDto.java
+++ src/main/java/com/rahul/verma/movierental/dto/ResponseDto.java
@@ -2,6 +2,7 @@ package com.rahul.verma.movierental.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.*;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResponseDto<T> {
+    @JsonUnwrapped
     T data;
     List<MessageDto> messages;
     Integer statusCode;
